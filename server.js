29f4984cf6b35fb863da945ff17bb804b296429b
JACOB CAPSHAW-TORRES <jacobcapshaw@gmail.com>
2016-12-18 16:47:32 -0500

    change mongo setup

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://127.0.0.1/recipe-api';
-var db = mongoose.connection;
-mongoose.connect(mongoUri);
+// var db = mongoose.connection;
+// mongoose.connect(mongoUri);
 
 var port = process.env.PORT || 5000;
 var router = express.Router();
@@ -15,7 +15,32 @@ var initialize = require('./app/initializeData');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', router);
-app.use(express.static('app'))
+app.use(express.static('app'));
+
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongoose.connect(mongoUri, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+
+
 
 router.use(function (req, res, next) {
   console.log('Something is happening.', port);
